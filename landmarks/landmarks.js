493a9a9bdf3b8c53b93722113b593b7186b8dab6
Theodore Tan <theodoretry@gmail.com>
2016-03-07 19:21:46 -0500

    Successfully received JSON

--- landmarks/landmarks.js
+++ landmarks/landmarks.js
@@ -16,20 +16,36 @@ var window = new google.maps.InfoWindow();
 
 function init() {
         map = new google.maps.Map(document.getElementById("map"), setup);
-        getMyLocation();
+        setMyLocation();
 }
 
-function getMyLocation() {
+function setMyLocation() {
         if (navigator.geolocation) { 
                 navigator.geolocation.getCurrentPosition(function(position) {
                         myLat = position.coords.latitude;
                         myLong = position.coords.longitude;
+                        parse();
                         renderMap();
                 });
         }
         else {
                 alert("Geolocation is not supported.");
         }
+}
+
+function parse() {
+        params = "login=RACHAEL_EDWARDS&lat=" + myLat + "&lng=" + myLong;
+        request.open("POST", "https://defense-in-derpth.herokuapp.com/sendLocation", true);
+        request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+        request.onreadystatechange = function() {
+                if (request.readyState == 4) {
+                        data = request.responseText;
+                        locations = JSON.parse(data);
+                        console.log(locations); // DEBUG
+                }
+        };
+        request.send(params);
 }
 
 function renderMap() {
